1dbb701e393f6b1f424cba8a0802479b098b5fb5
Buzz-Bazz <icoy338@gmail.com>
2025-11-20 22:54:41 +0700

    Push Logo + header

---
+++ components/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perpustakaan PWEB</title>
+    <link rel="icon" type="image/png" href="assets/img/Logo.png!sw800">
+</head>
+<body>
+    <header class="header">
+        <div class="container header-content">
+            <div class="logo">
+                <h1>Perpustakaan PWEB</h1>
+                <p class="tagline">Perpustakaan PWEB</p>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
