e7d5e7926d29e80196753ec9174d8925caf476df
Made Khanaya Dew Astawa <khanaydew123@gmail.com>
2025-11-20 21:54:43 +0700

    edit login

--- login.php
+++ login.php
@@ -1,27 +1,33 @@
 <?php
 session_start();
-require 'koneksi.php';
-
+require 'koneksiDB.php';
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $stmt = $koneksi->prepare("SELECT * FROM user WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
 
-    $q_user = mysqli_query($koneksi, "SELECT * FROM user WHERE username='$username' AND password='$password'");
+    $result = $stmt->get_result();
 
-    if (mysqli_num_rows($q_user) > 0) {
+    if ($result->num_rows > 0) {
         $_SESSION['username'] = $username;
         
-        $q_admin = mysqli_query($koneksi, "SELECT * FROM admin WHERE username='$username'");
-        
+        $q_admin = $koneksi->prepare("SELECT * FROM admin WHERE username=?");
+        $q_admin->bind_param("s", $username)
         if (mysqli_num_rows($q_admin) > 0) {
             $_SESSION['role'] = 'admin';
             header("Location: admin.php");
+            exit;
         } else {
             $_SESSION['role'] = 'anggota';
+            header("Location: index.php"); 
+            exit;
         }
     } else {
         echo "<script>
-            alert('Gagal Login');
+            alert('Username atau Password Salah');
+            window.location = 'login.php';
         </script>";
     }
 }
