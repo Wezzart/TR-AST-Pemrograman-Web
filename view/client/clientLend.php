eceadec6b36d46dc6a9af2183c5d3632a4ba72b5
Buzz-Bazz <icoy338@gmail.com>
2025-11-23 12:59:14 +0700

    rename directory

--- view/client/clientLend.php
+++ view/client/clientLend.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perpustakaan PWEB</title>
     <link rel="icon" type="image/png" href="assets/img/Logo.png!sw800">
-    <?php include '../../components/client/userHeader.php'; ?>
+    <?php include '../../components/client/clientHeader.php'; ?>
+    <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 <body>
     <p>Jokow</p>
