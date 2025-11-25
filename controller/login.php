e6859c4a9275ee32dacdd021256e880b71d4eb69
Buzz-Bazz <icoy338@gmail.com>
2025-11-25 23:08:55 +0700

    nambahin login dikit cuma html buat mastiin doang

--- controller/login.php
+++ controller/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require 'koneksiDB.php';
+require '../config/koneksiDB.php';
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -18,11 +18,11 @@ if (isset($_POST['login'])) {
         $q_admin->execute();
         if (mysqli_num_rows($q_admin) > 0) {
             $_SESSION['role'] = 'admin';
-            header("Location: admin.php");
+            header("Location: ../view/admin/adminHome.php");
             exit;
         } else {
             $_SESSION['role'] = 'anggota';
-            header("Location: index.php"); 
+            header("Location: ../view/client/clientHome.php"); 
             exit;
         }
     } else {
@@ -33,3 +33,26 @@ if (isset($_POST['login'])) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Anggota & Admin - Perpustakaan PWEB</title>
+    
+    <link rel="stylesheet" href="../assets/css/page/login.css"> 
+    <link rel="stylesheet" href="../assets/css/style.css">
+    </head>
+<body>
+    <div class="login-container">
+        <h2>LOGIN PERPUSTAKAAN</h2>
+        <form method="POST" action="login.php">
+            <input type="text" name="username" class="login-input" placeholder="Username" required>
+            <input type="password" name="password" class="login-input" placeholder="Password" required>
+            <button type="submit" name="login" class="login-button">Masuk</button>
+        </form>
+        <a href="#" class="register-link">Belum punya akun? Daftar di sini.</a>
+    </div>
+</body>
+</html>
