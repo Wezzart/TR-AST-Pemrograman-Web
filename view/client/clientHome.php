6ad6ac6614fdb38bd8d21154d7ef46b802a886f3
Made Khanaya Dew Astawa <khanaydew123@gmail.com>
2025-11-30 11:45:45 +0700

    fix security feature

--- view/client/clientHome.php
+++ view/client/clientHome.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+require '../../config/koneksiDB.php';
+
+if(!isset($_SESSION['role']) || $_SESSION['role'] != 'anggota'){
+     header("Location: ../login.php");
+     exit;
+ }
+?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
